381b595fbea4af3d13ca03ba53f0e9c5b5c503ac
Craft <38158429+Craft485@users.noreply.github.com>
2021-10-25 13:58:49 -0400

    Add Home.svelte and upated controller

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -2,10 +2,17 @@ import { Controller, Get, Logger, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  @Get(['/home', '/'])
+  @Render('Home')
+  getIndex() {
+    Logger.log('GET request received for /home or /', 'AppController');
+    return { titleCard: 'Hello World' };
+  }
+
   @Get('/chat')
   @Render('Chat')
   getChat() {
     Logger.log('GET request received for /chat', 'AppController');
-    return { message: 'Hello World!' };
+    return;
   }
 }
---
+++ views/Home.svelte
@@ -0,0 +1,14 @@
+<script>
+    // Setup Svelte
+    import Layout from './Layout.svelte';
+    export let titleCard;
+</script>
+
+<svelte:head>
+    <!-- Head Content -->
+    <title>Home</title>
+</svelte:head>
+
+<Layout>
+    <h1>{ titleCard }</h1>
+</Layout>
