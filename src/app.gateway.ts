7e474cc0b9e41e1312f6453813684fde7570f6f0
Craft <38158429+Craft485@users.noreply.github.com>
2021-10-25 12:56:06 -0400

    handle ws events server side

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, Render } from '@nestjs/common';
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
@@ -20,8 +20,9 @@ export class AppGateway
     this.logger.log('Inittialized!');
   }
 
-  handleConnection(client: Socket, ...args: any[]) {
-    this.logger.log(`Client connected: ${client.id}`);
+  handleConnection(socket: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${socket.id}`);
+    socket.join('room1');
   }
 
   handleDisconnect(client: Socket) {
@@ -29,7 +30,13 @@ export class AppGateway
   }
 
   @SubscribeMessage('msgToServer')
-  handleMessage(client: Socket, text: string): WsResponse<string> {
-    return { event: 'msgFromServer', data: text };
+  handleMessage(
+    client: Socket,
+    text: string,
+  ): WsResponse<string | unknown> | void {
+    const m = `<div class="msg">${text}</div><br/>`;
+    // Using the 'to' method here means that the client that sent the message will be excluded from the server emitted event, whereas the 'in' method will send it to absolutly everyone in the room
+    client.in('room1').emit('msgFromServer', m);
+    return;
   }
 }
